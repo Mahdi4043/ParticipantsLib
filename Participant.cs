36f5cb80f0a608cc54adad165365555d81a11634
Matti Barlach <mattibarlach@gmail.com>
2024-12-19 12:44:51 +0100

    Add project files.

---
+++ Participant.cs
@@ -0,0 +1,46 @@
+﻿namespace ParticipantsLib
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Max Children: {Age}, Minimum Age: {Country}";
+        }
+        public void ValidateName()
+        {
+            if (Name.Trim().Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("name needs to be at least 2 characters long");
+            }
+        }
+        public void ValidateAge()
+        {
+            if (Age < 12)
+            {
+                throw new ArgumentOutOfRangeException("Age needs to be at least 12");
+            }
+        }
+        public void ValidateCountry()
+        {
+            if (Country.Trim().Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("Country needs to be at least 3 characters long");
+            }
+        }
+        public void Validate()
+        {
+            ValidateName();
+            ValidateAge();
+            ValidateCountry();
+        }
+    }
+
+}
+
---
+++ ParticipantsRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticipantsLib
+{
+    public class ParticipantsRepository
+    {
+        private int _nextId = 1;
+        private List<Participant> partis = new List<Participant>();
+
+        public ParticipantsRepository()
+        {
+            Participant tobias = new Participant() { Name = "Tobias", Age = 12, Country = "USA" };
+            Participant viktor = new Participant() { Name = "Viktor", Age = 13, Country = "Denmark" };
+            Participant oliver = new Participant() { Name = "Oliver", Age = 14, Country = "Britain" };
+            Participant jonas = new Participant() { Name = "Jonas", Age = 15, Country = "Egypt" };
+            Participant matti = new Participant() { Name = "Matti", Age = 16, Country = "Sweden" };
+            AddParti(tobias);
+            AddParti(viktor);
+            AddParti(oliver);
+            AddParti(jonas);
+            AddParti(matti);
+        }
+
+        public IEnumerable<Participant> GetAll()
+        {
+            IEnumerable<Participant> result = new List<Participant>(partis);
+            return result;
+        }
+
+        public Participant GetById(int id)
+        {
+
+            Participant? parti = partis.FirstOrDefault(p => p.Id == id);
+
+            if (parti == null)
+            {
+                return null;
+            }
+            else
+                return parti;
+        }
+
+        public Participant AddParti(Participant parti)
+        {
+
+            parti.Validate();
+            parti.Id = _nextId++;
+            partis.Add(parti);
+            return parti;
+        }
+
+        public Participant? Delete(int id)
+        {
+            partis.Remove(GetById(id));
+            return GetById(id);
+        }
+
+    }
+}
